18e745bc6cec67132be52acc6f2ec47a8b5b2b99
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-05 19:10:00 +0530

    updated model 88% accurate

--- model.py
+++ model.py
@@ -12,28 +12,20 @@ train_dir = 'asl_split/train'
 test_dir = 'asl_split/test'
 
 # Define the data generators for training and validation
-train_datagen = ImageDataGenerator(rescale=1./255,
-                                   rotation_range=20,
-                                   width_shift_range=0.2,
-                                   height_shift_range=0.2,
-                                   shear_range=0.2,
-                                   zoom_range=0.2,
-                                   horizontal_flip=True,
-                                   fill_mode='nearest')
+train_datagen = ImageDataGenerator(rescale=1./255)
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(train_dir,
                                                     target_size=(224, 224),
                                                     batch_size=32,
-                                                    class_mode='categorical',                                            
-                                                    seed =42)
+                                                    class_mode='categorical'                                          
+                                                    )
 test_generator = test_datagen.flow_from_directory(test_dir,
                                                   target_size=(224, 224),
                                                   batch_size=32,
-                                                  class_mode='categorical',
-                                                  shuffle=False,
-                                                  seed=42)
+                                                  class_mode='categorical'
+                                                  )
 
 # Define the MobileNetV2 model as the base model
 base_model = MobileNetV2(
