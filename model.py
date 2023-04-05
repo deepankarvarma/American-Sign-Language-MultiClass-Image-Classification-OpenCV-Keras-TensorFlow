1b5d94b6c4225f5c5c02a26ac4b27420eaa4e37c
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-05 20:08:19 +0530

    model with 91% accuracy

--- model.py
+++ model.py
@@ -3,35 +3,53 @@ import os
 import shutil
 import random
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.layers import BatchNormalization, Dense, Dropout
 from tensorflow.keras import Model, Input
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.optimizers import Adam
 # Define the training and test data directories
-train_dir = 'asl_split/train'
-test_dir = 'asl_split/test'
-
+data_dir='asl_dataset'
 # Define the data generators for training and validation
-train_datagen = ImageDataGenerator(rescale=1./255)
+datagen = ImageDataGenerator(
+        rescale = 1./255,
+        rotation_range = 40,
+        width_shift_range = 0.2,
+        height_shift_range = 0.2,
+        shear_range = 0.2,
+        zoom_range = 0.2,
+        horizontal_flip = True,
+        fill_mode = 'nearest',
+        validation_split = 0.2)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(train_dir,
-                                                    target_size=(224, 224),
-                                                    batch_size=32,
-                                                    class_mode='categorical'                                          
-                                                    )
-test_generator = test_datagen.flow_from_directory(test_dir,
-                                                  target_size=(224, 224),
-                                                  batch_size=32,
-                                                  class_mode='categorical'
-                                                  )
+height = 224
+width = 224
+channels = 3
+batch_size = 32
+img_shape = (height, width, channels)
+img_size = (height, width)
+
+
+train_data = datagen.flow_from_directory(
+    data_dir,
+    target_size = img_size,
+    batch_size = batch_size,
+    class_mode = 'categorical',
+    subset = 'training')
+
+val_data = datagen.flow_from_directory(
+    data_dir,
+    target_size = img_size,
+    batch_size = batch_size,
+    class_mode='categorical',
+    subset = 'validation')
 
 # Define the MobileNetV2 model as the base model
 base_model = MobileNetV2(
     input_shape=(224, 224, 3),
     include_top=False,
-    weights='imagenet'
+    weights='imagenet',
+    pooling='avg'
 )
 base_model.trainable = False
 
@@ -41,7 +59,8 @@ for layer in base_model.layers:
 
 # Combine the base model with the new layers
 x = base_model.output
-x = GlobalAveragePooling2D()(x)
+x = BatchNormalization()(x)
+x = Dense(1024, activation='relu')(x)
 x = Dropout(rate=0.2)(x)
 outputs = Dense(units=36, activation="softmax")(x)
 model = Model(inputs=base_model.input, outputs=outputs)
@@ -53,13 +72,15 @@ model.compile(optimizer=Adam(learning_rate=LEARNING_RATE),
               metrics=['accuracy'])
 
 # Train the model
-history = model.fit(train_generator,
-                    epochs=15,
-                    validation_data=test_generator)
+STEP_SIZE_TRAIN = train_data.n // train_data.batch_size
+STEP_SIZE_VALID = val_data.n // val_data.batch_size
 
-# Evaluate the model on the test set
-test_loss, test_acc = model.evaluate(test_generator)
-print('Test accuracy:', test_acc)
+history = model.fit_generator(train_data,
+                    steps_per_epoch = STEP_SIZE_TRAIN,
+                    validation_data = val_data,
+                    validation_steps = STEP_SIZE_VALID,
+                    epochs = 30,
+                    verbose = 1)
 
 # Saving the model
 model.save('asl_image_classification.h5')
